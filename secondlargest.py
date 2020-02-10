e1c067dcec27f8e758b7cbfa6e2587c7496572e9
dr4gonw4ll <anand.namana@gmail.com>
2020-02-09 22:32:57 -0500

    Test code

--- secondlargest.py
+++ secondlargest.py
@@ -1,4 +1,4 @@
-def letsort(li):
+def letsort(li): #sorting function, not so efficient
     temp=0;
     for i in range(0,len(li)):
         for j in range(i,len(li)):
@@ -6,16 +6,16 @@ def letsort(li):
                 temp=li[j]
                 li[j]=li[i]
                 li[i]=temp
-    return(l);
+    return(li);
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     n = int(input())
-    arr = map(int,input().split())
+    arr = map(int,input().split())   #input split if into one line
     l = list(arr)
     li1 = letsort(l)
     max = li1[0];
-    ar = map(lambda x:x-max,li1)
+    ar = map(lambda x:x-max,li1) #use anonymous function lambda to subtract the max value
     newar = list(ar)
     for k in range(0,len(newar)):
         if(newar[k]!=0):
